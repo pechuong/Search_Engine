f8aff8db0266a8981619f3f62c5bc2cc7668d0e9
peter <peter@10.0.0.31>
2018-09-06 00:34:07 -0700

    Made an attempt at getting the output file if it exists

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 /**
@@ -10,13 +12,13 @@ public class Driver {
 	
 	private HashMap<String, HashMap<String, Integer>> invertedIndex;
 	private HashMap<String, String> ArgMap;
+	private static boolean outputting = false;
 
 	/**
 	 * Parses the command-line arguments to build and use an in-memory search
 	 * engine from files or the web.
 	 *
 	 * @param args the command-line arguments to parse
-	 * @return 0 if everything went well
 	 */
 	public static void main(String[] args) {
 		// TODO Fill in
@@ -46,9 +48,48 @@ public class Driver {
 		
 	}
 	//TODO Make a method for traversing a directory and listing all text files
+	public void listFiles(Path dir) {
+		
+	}
 	
+	/**
+	 * Figures out if given argument is a flag or not
+	 * 
+	 * @param arg the command-line argument given
+	 * @return true if it is a flag
+	 */
+	public static boolean isFlag(String arg) {
+		if (arg == null) {
+			return false;
+		}
+		return arg.matches("^-\\S+");
+	}
 	
-	
+	/**
+	 * Sees if there is a "-index" flag
+	 * 
+	 * @param args the command-line argument given
+	 * @return true if that flag is found
+	 */
+	public static Path getOutput(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].matches("^-index")) {
+				try {
+					if (!isFlag(args[i + 1])) {
+						return Paths.get(args[i + 1]);
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					return null;
+				}
+			}
+		}
+		/*for (String arg : args) {
+			if (arg.matches("^-index")) {
+				return true;
+			}
+		}
+		return false;*/
+	}
 	
 	/**
 	 * Determines if given argument is a text file
@@ -73,19 +114,5 @@ public class Driver {
 		}
 		return !isText(arg);
 	}
-	
-	/**
-	 * Figures out if given argument is a flag or not
-	 * 
-	 * @param arg the command-line argument given
-	 * @return true if it is a flag
-	 */
-	public static boolean isFlag(String arg) {
-		if (arg == null) {
-			return false;
-		}
-		return arg.matches("^-\\S+");
-	}
-	
 
 }
