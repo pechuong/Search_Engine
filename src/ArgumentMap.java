da87d907644dfe80d4224365276c2e4fa382b45f
peter <peter@10.0.0.31>
2018-09-10 21:50:31 -0700

    Removed methods and added two hw files to directory

---
+++ src/ArgumentMap.java
@@ -0,0 +1,200 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgumentMap {
+
+	private final Map<String, String> map;
+
+	/**
+	 * Initializes this argument map.
+	 */
+	public ArgumentMap() {
+		this.map = new HashMap<String, String>(); // DONE Properly initialize this.
+	}
+
+	/**
+	 * Initializes this argument map and then parsers the arguments into
+	 * flag/value pairs where possible. Some flags may not have associated values.
+	 * If a flag is repeated, its value is overwritten.
+	 *
+	 * @param args
+	 */
+	public ArgumentMap(String[] args) {
+		this();
+		parse(args);
+	}
+
+	/**
+	 * Parses the arguments into flag/value pairs where possible. Some flags may
+	 * not have associated values. If a flag is repeated, its value is
+	 * overwritten.
+	 *
+	 * @param args the command line arguments to parse
+	 */
+	public void parse(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			if (isValidFlag(args[i]) || hasFlag(args[i])) {
+				try {
+					if (isValue(args[i+1])) {
+						this.map.put(args[i], args[i+1]);
+					} else {
+						this.map.put(args[i], null);
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					this.map.put(args[i], null);
+				}
+			}
+		}
+		// DONE Fill in this method. 
+	}
+
+	/**
+	 * Determines whether the argument is a flag. Flags start with a dash "-"
+	 * character, followed by at least one other non-whitespace character.
+	 *
+	 * @param arg the argument to test if its a flag
+	 * @return {@code true} if the argument is a flag
+	 *
+	 * @see String#startsWith(String)
+	 * @see String#trim()
+	 * @see String#isEmpty()
+	 * @see String#length()
+	 */
+	public static boolean isValidFlag(String arg) {
+		if (arg == null) {
+			return false;
+		}
+		return arg.matches("^-index") | arg.matches("^-path");
+	}
+
+	/**
+	 * Determines whether the argument is a value. Values do not start with a dash
+	 * "-" character, and must consist of at least one non-whitespace character.
+	 *
+	 * @param arg the argument to test if its a value
+	 * @return {@code true} if the argument is a value
+	 *
+	 * @see String#startsWith(String)
+	 * @see String#trim()
+	 * @see String#isEmpty()
+	 * @see String#length()
+	 */
+	public static boolean isValue(String arg) {
+		//return arg.matches("[^-]\\S+.*");   // test | old code
+		if (arg == null) {
+			return false;
+		}
+		return arg.matches("[^-\\s]+.*");
+
+		//DONE Fill in this method and fix the return. 
+	}
+
+	/**
+	 * Returns the number of unique flags.
+	 *
+	 * @return number of unique flags
+	 */
+	public int numFlags() {
+		return this.map.keySet().size();
+		//DONE Fill in this method and fix the return. 
+	}
+
+	/**
+	 * Determines whether the specified flag exists.
+	 *
+	 * @param flag the flag to search for
+	 * @return {@code true} if the flag exists
+	 */
+	public boolean hasFlag(String flag) {
+		return this.map.containsKey(flag);
+	}
+
+	/**
+	 * Determines whether the specified flag is mapped to a non-null value.
+	 *
+	 * @param flag the flag to search for
+	 * @return {@code true} if the flag is mapped to a non-null value
+	 */
+	public boolean hasValue(String flag) {
+		return this.map.get(flag) != null;
+		//DONE Fill in this method and fix the return. 
+	}
+
+	/**
+	 * Returns the value to which the specified flag is mapped as a
+	 * {@link String}, or null if there is no mapping for the flag.
+	 *
+	 * @param flag the flag whose associated value is to be returned
+	 * @return the value to which the specified flag is mapped, or {@code null} if
+	 *         there is no mapping for the flag
+	 */
+	public String getString(String flag) {
+		//System.out.printf("My flag is %s and value is %s\n", flag, this.map.get(flag));  //testing values
+		return this.map.get(flag);
+		//DONE Fill in this method and fix the return. 
+	}
+
+	/**
+	 * Returns the value to which the specified flag is mapped as a
+	 * {@link String}, or the default value if there is no mapping for the flag.
+	 *
+	 * @param flag         the flag whose associated value is to be returned
+	 * @param defaultValue the default value to return if there is no mapping for
+	 *                     the flag
+	 * @return the value to which the specified flag is mapped, or the default
+	 *         value if there is no mapping for the flag
+	 */
+	public String getString(String flag, String defaultValue) {
+		String myValue = getString(flag);
+		return (myValue != null)?myValue:defaultValue;
+		//DONE Fill in this method and fix the return. 
+	}
+
+	/**
+	 * Returns the value to which the specified flag is mapped as a {@link Path},
+	 * or {@code null} if unable to retrieve this mapping for any reason
+	 * (including being unable to convert the value to a {@link Path} or no value
+	 * existing for this flag).
+	 *
+	 * This method should not throw any exceptions!
+	 *
+	 * @param flag the flag whose associated value is to be returned
+	 * @return the value to which the specified flag is mapped, or {@code null} if
+	 *         unable to retrieve this mapping for any reason
+	 *
+	 * @see Paths#get(String, String...)
+	 */
+	public Path getPath(String flag) {
+		var myValue = getString(flag);
+		//System.out.printf("myValue is %d\n", myValue);
+		return (myValue != null)?Paths.get(myValue):null;
+		//DONE Fill in this method and fix the return. 
+	}
+
+	/**
+	 * Returns the value to which the specified flag is mapped as a {@link Path},
+	 * or the default value if unable to retrieve this mapping for any reason
+	 * (including being unable to convert the value to a {@link Path} or no value
+	 * existing for this flag).
+	 *
+	 * This method should not throw any exceptions!
+	 *
+	 * @param flag         the flag whose associated value is to be returned
+	 * @param defaultValue the default value to return if there is no mapping for
+	 *                     the flag
+	 * @return the value to which the specified flag is mapped as a {@link Path},
+	 *         or the default value if there is no mapping for the flag
+	 */
+	public Path getPath(String flag, Path defaultValue) {
+		var myPath = getPath(flag);
+		return (myPath != null)?myPath:defaultValue;
+		//DONE Fill in this method and fix the return. 
+	}
+
+	@Override
+	public String toString() {
+		return this.map.toString();
+	}
+}
--- src/Driver.java
+++ src/Driver.java
@@ -10,7 +10,7 @@ import java.nio.file.Paths;
  */
 public class Driver {
 	
-	private HashMap<String, HashMap<String, Integer>> invertedIndex;
+	private final HashMap<String, HashMap<String, Integer>> invertedIndex;
 	private HashMap<String, String> ArgMap;
 	private static boolean outputting = false;
 
@@ -23,7 +23,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Fill in
 		System.out.println(Arrays.toString(args));
-		parseArgs(args);
+		//parseArgs(args);
 	}
 	
 	/**
@@ -34,62 +34,18 @@ public class Driver {
 		this.ArgMap = new HashMap<>();
 	}
 	
-	/**
-	 * 
-	 * @param args
-	 */
-	public static void parseArgs(String[] args) {
-		//TODO detect flag / value and pair them in argMap
-		
-		//TODO 
-		
-		//TODO turn the paths into Path objects w/ get(String path)
-		//(maybe in another method)
-		
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//TODO Make a method for traversing a directory and listing all text files
 	public void listFiles(Path dir) {
 		
-	}
-	
-	/**
-	 * Figures out if given argument is a flag or not
-	 * 
-	 * @param arg the command-line argument given
-	 * @return true if it is a flag
-	 */
-	public static boolean isFlag(String arg) {
-		if (arg == null) {
-			return false;
-		}
-		return arg.matches("^-\\S+");
-	}
-	
-	/**
-	 * Sees if there is a "-index" flag
-	 * 
-	 * @param args the command-line argument given
-	 * @return true if that flag is found
-	 */
-	public static Path getOutput(String[] args) {
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].matches("^-index")) {
-				try {
-					if (!isFlag(args[i + 1])) {
-						return Paths.get(args[i + 1]);
-					}
-				} catch (ArrayIndexOutOfBoundsException e) {
-					return null;
-				}
-			}
-		}
-		return null;
-		/*for (String arg : args) {
-			if (arg.matches("^-index")) {
-				return true;
-			}
-		}
-		return false;*/
 	}
 	
 	/**
