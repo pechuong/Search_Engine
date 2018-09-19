3129cc5a938cdab708c14c40f6aa7db8ddee3ee1
peter <peter@10.1.115.92>
2018-09-19 15:10:41 -0700

    Moved traverse out of class and into another

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,32 @@
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
+
+/*
+ * TODO
+ * Put anything generally-useful in its own class outside of Driver
+ * In Driver only handle project-specific stuff, just checking for specific flags and values
+ * 
+ * main {
+ *   ArgMap map...
+ *   
+ *   if (have the -path flag)
+ *      trigger building the index
+ *     
+ *   if (have the -index flag)
+ *       trigger writing the index
+ * 
+ * }
+ */
 
 public class Driver {
-	
+	// TODO Need to make this non-static, put this in its own data-structure-like class
+	// TODO Adding stuff to this data structure, toString(), other methods similar to what you found in WordIndex
+	// TODO Step 1: Create your own class but keep the static keyword, Step 2: Remove the static keyword
 	public static TreeMap<String, TreeMap<String, TreeSet<Integer>>> invertedIndex = new TreeMap<>();
-	public ArgumentMap ArgMap;
+	
+	public ArgumentMap ArgMap;  // TODO Make a local variable inside your main method and then remove the constructor
 
 	/**
 	 * Parses the command-line arguments to build and use an in-memory search
@@ -24,13 +42,13 @@ public class Driver {
 		try {
 			// If I don't get nothing from my -path flag, traverse it
 			if (test1.ArgMap.getPath("-path") != null) {
-				traverse(test1.ArgMap.getPath("-path"));
+				TraversePath.traverse(test1.ArgMap.getPath("-path"));
 			} else 
 				System.out.println("Didn't traverse path");
 		} catch (NullPointerException e){
 			System.out.println("No -path found :(");
 		} catch (IOException e) {
-			System.out.println("File doesn't exist");
+			System.out.printf("File: " + test1.ArgMap.getString("-path") + " does not exist!" + System.lineSeparator());
 		}
 		
 		// Outputs inverted index to json if output is true
@@ -62,57 +80,5 @@ public class Driver {
 		this.ArgMap = new ArgumentMap(args);
 	}
 	
-	/**
-	 * Outputs the name of the file or subdirectory, with proper indentation to
-	 * help indicate the hierarchy. If a subdirectory is encountered, will
-	 * recursively list all the files in that subdirectory. For each file found,
-	 * the function will stem that file into an inverted index
-	 *
-	 * The recursive version of this method is private. Users of this class will
-	 * have to use the public version (see below).
-	 *
-	 * @param prefix the padding or prefix to put infront of the file or
-	 *               subdirectory name
-	 * @param path   to retrieve the listing, assumes a directory and not a file
-	 *               is passed
-	 * @throws IOException
-	 */
-	private static void traverse(String prefix, Path path) throws IOException {
-		try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)) {
-			// looks thru directory
-			for (Path file : listing) {
-				//System.out.print(prefix + file.getFileName());
-				if (Files.isDirectory(file)) {
-					// Add a slash so we can tell it is a directory
-					//System.out.println("/");
-					traverse("  " + prefix, file);
-				} else {
-					// If file is a text file, stem it
-					if (file.toString().matches(".*[tT][eE][xX][tT]$") || file.toString().matches(".*[tT][xX][tT]$")) {
-						TextFileStemmer.stemFile(file);
-					}
-				}
-			}
-		} catch (IOException e) {
-			System.out.println("Files doesn't exist!");
-		}
-	}
-
-	/**
-	 * Safely starts the recursive traversal with the proper padding. Users of
-	 * this class can access this method, so some validation is required.
-	 *
-	 * @param directory to traverse
-	 * @throws IOException
-	 */
-	public static void traverse(Path directory) throws IOException {
-		// look thru path if directory
-		if (Files.isDirectory(directory)) {
-			traverse("- ", directory);
-		} else {
-			// otherwise stem file from path
-			System.out.println(directory.getFileName());
-			TextFileStemmer.stemFile(directory);
-		}
-	}
+	
 }
