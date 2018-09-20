19b28a1cbd98be0d8f9dbb750cf52d7ceb09fa32
peter <peter@10.1.115.92>
2018-09-19 22:25:21 -0700

    Removed the constructor and put arg map in main

--- src/Driver.java
+++ src/Driver.java
@@ -1,22 +1,22 @@
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /*
  * TODO
  * Put anything generally-useful in its own class outside of Driver
  * In Driver only handle project-specific stuff, just checking for specific flags and values
- * 
+ *
  * main {
  *   ArgMap map...
- *   
+ *
  *   if (have the -path flag)
  *      trigger building the index
- *     
+ *
  *   if (have the -index flag)
  *       trigger writing the index
- * 
+ *
  * }
  */
 
@@ -25,8 +25,6 @@ public class Driver {
 	// TODO Adding stuff to this data structure, toString(), other methods similar to what you found in WordIndex
 	// TODO Step 1: Create your own class but keep the static keyword, Step 2: Remove the static keyword
 	public static TreeMap<String, TreeMap<String, TreeSet<Integer>>> invertedIndex = new TreeMap<>();
-	
-	public ArgumentMap ArgMap;  // TODO Make a local variable inside your main method and then remove the constructor
 
 	/**
 	 * Parses the command-line arguments to build and use an in-memory search
@@ -36,34 +34,34 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 		// parses args
-		Driver test1 = new Driver(args);
-		
+		ArgumentMap ArgMap = new ArgumentMap(args);
+
 		// Traverses and makes inverted index
 		try {
 			// If I don't get nothing from my -path flag, traverse it
-			if (test1.ArgMap.getPath("-path") != null) {
-				TraversePath.traverse(test1.ArgMap.getPath("-path"));
-			} else 
+			if (ArgMap.getPath("-path") != null) {
+				TraversePath.traverse(ArgMap.getPath("-path"));
+			} else
 				System.out.println("Didn't traverse path");
 		} catch (NullPointerException e){
 			System.out.println("No -path found :(");
 		} catch (IOException e) {
-			System.out.printf("File: " + test1.ArgMap.getString("-path") + " does not exist!" + System.lineSeparator());
+			System.out.printf("File: " + ArgMap.getString("-path") + " does not exist!" + System.lineSeparator());
 		}
-		
+
 		// Outputs inverted index to json if output is true
-		if (test1.ArgMap.output == true) {
+		if (ArgMap.output == true) {
 			try {
-				// If -index flag doesn't give me a null 
-				if (test1.ArgMap.getString("-index") != null) {
+				// If -index flag doesn't give me a null
+				if (ArgMap.getString("-index") != null) {
 					// If the file doesn't exist, make that file
-					if (!Files.exists(test1.ArgMap.getPath("-index"))) {
-						Files.createFile(test1.ArgMap.getPath("-index"));
+					if (!Files.exists(ArgMap.getPath("-index"))) {
+						Files.createFile(ArgMap.getPath("-index"));
 					}
 					// Output my index to the provided json file
-					TreeJSONWriter.asObject(invertedIndex, test1.ArgMap.getPath("-index"));
+					TreeJSONWriter.asObject(invertedIndex, ArgMap.getPath("-index"));
 				}
-				else 
+				else
 					System.out.println("Didn't format inverted index... no output found");
 			} catch (IOException e) {
 				System.out.println("Error in opening Path given to 'asObject' method");
@@ -72,13 +70,12 @@ public class Driver {
 		// Avoids the index stacking up when the second args is run
 		invertedIndex.clear();
 	}
-	
-	/**
-	 * Instantiates Driver and makes a new ArgMap
-	 */
-	public Driver(String[] args){
-		this.ArgMap = new ArgumentMap(args);
+
+	/*
+	@Override
+	public String toString() {
+		return ;
 	}
-	
-	
+	 */
 }
+
