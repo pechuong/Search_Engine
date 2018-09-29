1ec459de3e06b3d2151a5f5a34c30d718293386f
peter <peter@10.0.0.31>
2018-09-28 23:41:01 -0700

    Made two hasFile method and added javadoc comments to them

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -22,5 +22,31 @@ public class InvertedIndex {
 		return iIndex.containsKey(word.toLowerCase());
 	}
 
+	/**
+	 * Checks if the indexed word has the given path
+	 *
+	 * @param word The word index to find the path
+	 * @param path The path to be found
+	 * @return true if the path exists in the specific word's index
+	 */
+	public boolean hasFile(String word, String path) {
+		return iIndex.get(word).containsKey(path);
+	}
+
+	/**
+	 * Checks the whole index for the path and if it
+	 * find one, then it returns true
+	 *
+	 * @param path The path to be found
+	 * @return true if the path exists in the inverted index
+	 */
+	public boolean hasFile(String path) {
+		for (String word : iIndex.keySet()) {
+			if (hasFile(word, path)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
