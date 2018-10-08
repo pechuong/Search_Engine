090168b31550c5d604f708202b23307101c9a9bc
peter <peter@10.0.0.31>
2018-10-08 01:06:10 -0700

    Added javadoc for hasFile and buildLocation

--- src/LocationMap.java
+++ src/LocationMap.java
@@ -5,14 +5,29 @@ public class LocationMap {
 
 	private final TreeMap<String, Integer> location;
 
+	/**
+	 * Initializes the location
+	 */
 	public LocationMap() {
 		this.location = new TreeMap<>();
 	}
 
+	/**
+	 * Checks if the location Map has this file
+	 *
+	 * @param path The file to search for in the map
+	 * @return true if the file was found
+	 */
 	public boolean hasFile(String path) {
 		return this.location.containsKey(path);
 	}
 
+	/**
+	 * Builds the locationMap further and adds to it
+	 *
+	 * @param inputFile The file to add to the map
+	 * @param wordCount The word count to associate w/ file
+	 */
 	public void buildLocation(Path inputFile, int wordCount) {
 		add(inputFile.toString(), wordCount);
 	}
