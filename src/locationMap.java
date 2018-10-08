f08482347267ff94b73fd19767352801aa05ee30
peter <peter@10.0.0.31>
2018-10-08 00:41:01 -0700

    Added methods for adding location and their word count

--- src/locationMap.java
+++ src/locationMap.java
@@ -5,7 +5,26 @@ public class locationMap {
 	private final TreeMap<String, Integer> location;
 
 	public locationMap() {
-		location = new TreeMap<>();
+		this.location = new TreeMap<>();
 	}
 
+	public boolean hasFile(String path) {
+		return this.location.containsKey(path);
+	}
+
+	private void add(String path, int amount) {
+		if (!hasFile(path)) {
+			addFile(path);
+		}
+		addWordCount(path, amount);
+
+	}
+
+	private void addFile(String path) {
+		this.location.put(path, 0);
+	}
+
+	private void addWordCount(String path, int amount) {
+		this.location.put(path, this.location.get(path) + amount);
+	}
 }
