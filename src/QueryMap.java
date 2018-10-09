7343ee357adbbb17117ec79a6224fc76b1c1773f
peter <peter@10.0.0.31>
2018-10-09 00:59:53 -0700

    Map a queryMap class to store the search results

---
+++ src/QueryMap.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.TreeMap;
+
+public class QueryMap {
+
+	private final TreeMap<String, List<Result>> queryMap;
+
+	public QueryMap() {
+		this.queryMap = new TreeMap<>();
+	}
+
+	public void addQuery(String path, List<Result> results) {
+		this.queryMap.put(path, results);
+	}
+
+}
