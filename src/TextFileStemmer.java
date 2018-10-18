785885620a05f845ef73c5ff1e75a7d0349089b6
peter <peter@10.1.27.200>
2018-10-18 16:28:02 -0700

    Renamed iIndex to index and made a reusable stemmer in stemFile method

--- src/TextFileStemmer.java
+++ src/TextFileStemmer.java
@@ -44,7 +44,6 @@ public class TextFileStemmer {
 	}
 
 	// TODO Move into the TraversePath class
-	// TODO Rename iIndex to just index
 	/**
 	 * Reads a file line by line, parses each line into cleaned and stemmed words,
 	 * and then writes that line to a new file.
@@ -56,33 +55,33 @@ public class TextFileStemmer {
 	 * @see #stemLine(String)
 	 * @see TextParser#parse(String)
 	 */
-	public static void stemFile(InvertedIndex iIndex, Path inputFile) throws IOException {
+	public static void stemFile(InvertedIndex index, Path inputFile) throws IOException {
 		try (
 				var reader = Files.newBufferedReader(inputFile, StandardCharsets.UTF_8);
 				) {
 			String line;
-			
+			Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
 			/*
 			 * TODO Some efficiency issues
 			 * 1) Any time you use "temporary storage" like this list, and then
 			 * move data elsewhere (like into your index), using more space and
 			 * time than necessary.
-			 * 
+			 *
 			 * Efficiency is one reason to create a more specific version of
 			 * generalized code. Copy/paste some of what is going on in stemLine
 			 * into your while loop... and instead of adding to a list immediately
 			 * add to your index
-			 * 
+			 *
 			 * 2) Creating a stemmer object for every line, causing garbage collection
 			 * Instead, create 1 stemmer per file.
 			 */
-			
+
 			ArrayList<String> wordList = new ArrayList<>();
 			while ((line = reader.readLine()) != null) {
-				List<String> stemmed = stemLine(line);
+				List<String> stemmed = stemLine(line, stemmer);
 				wordList.addAll(stemmed);
 			}
-			iIndex.buildiIndex(wordList, inputFile);
+			index.buildiIndex(wordList, inputFile);
 		}
 	}
 
