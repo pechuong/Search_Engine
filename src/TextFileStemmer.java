505a9d0223de9f5d816ad8cd7fbbbf4dbb288369
peter <peter@10.1.27.200>
2018-10-18 16:56:19 -0700

    Moved stemFile to TraversePath and removed some TODO comments

--- src/TextFileStemmer.java
+++ src/TextFileStemmer.java
@@ -1,7 +1,3 @@
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,46 +37,6 @@ public class TextFileStemmer {
 			wordList.add(stemmer.stem(word).toString());
 		}
 		return wordList;
-	}
-
-	// TODO Move into the TraversePath class
-	/**
-	 * Reads a file line by line, parses each line into cleaned and stemmed words,
-	 * and then writes that line to a new file.
-	 *
-	 * @param inputFile the input file to parse
-	 * @param outputFile the output file to write the cleaned and stemmed words
-	 * @throws IOException if unable to read or write to file
-	 *
-	 * @see #stemLine(String)
-	 * @see TextParser#parse(String)
-	 */
-	public static void stemFile(InvertedIndex index, Path inputFile) throws IOException {
-		try (
-				var reader = Files.newBufferedReader(inputFile, StandardCharsets.UTF_8);
-				) {
-			String line;
-			Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
-			/*
-			 * TODO Some efficiency issues
-			 * 1) Any time you use "temporary storage" like this list, and then
-			 * move data elsewhere (like into your index), using more space and
-			 * time than necessary.
-			 *
-			 * Efficiency is one reason to create a more specific version of
-			 * generalized code. Copy/paste some of what is going on in stemLine
-			 * into your while loop... and instead of adding to a list immediately
-			 * add to your index
-			 *
-			 */
-
-			ArrayList<String> wordList = new ArrayList<>();
-			while ((line = reader.readLine()) != null) {
-				List<String> stemmed = stemLine(line, stemmer);
-				wordList.addAll(stemmed);
-			}
-			index.buildiIndex(wordList, inputFile);
-		}
 	}
 
 }
--- src/TraversePath.java
+++ src/TraversePath.java
@@ -1,9 +1,14 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
-// TODO Move stemFile into this class
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+
 // TODO Maybe refactor the class name to InvertedIndexBuilder
 
 public class TraversePath {
@@ -22,7 +27,46 @@ public class TraversePath {
 				}
 			}
 		} else if (path.toString().matches("(?i).*\\.te?xt$")) {
-			TextFileStemmer.stemFile(index, path);
+			stemFile(index, path);
+		}
+	}
+
+	/**
+	 * Reads a file line by line, parses each line into cleaned and stemmed words,
+	 * and then writes that line to a new file.
+	 *
+	 * @param inputFile the input file to parse
+	 * @param outputFile the output file to write the cleaned and stemmed words
+	 * @throws IOException if unable to read or write to file
+	 *
+	 * @see #stemLine(String)
+	 * @see TextParser#parse(String)
+	 */
+	public static void stemFile(InvertedIndex index, Path inputFile) throws IOException {
+		try (
+				var reader = Files.newBufferedReader(inputFile, StandardCharsets.UTF_8);
+				) {
+			String line;
+			Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
+			/*
+			 * TODO Some efficiency issues
+			 * 1) Any time you use "temporary storage" like this list, and then
+			 * move data elsewhere (like into your index), using more space and
+			 * time than necessary.
+			 *
+			 * Efficiency is one reason to create a more specific version of
+			 * generalized code. Copy/paste some of what is going on in stemLine
+			 * into your while loop... and instead of adding to a list immediately
+			 * add to your index
+			 *
+			 */
+
+			ArrayList<String> wordList = new ArrayList<>();
+			while ((line = reader.readLine()) != null) {
+				List<String> stemmed = TextFileStemmer.stemLine(line, stemmer);
+				wordList.addAll(stemmed);
+			}
+			index.buildiIndex(wordList, inputFile);
 		}
 	}
 
