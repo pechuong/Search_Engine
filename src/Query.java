71900b62267c279f47ebdaf0dfc7b74dfc8730c4
peter <peter@10.0.0.6>
2018-11-17 21:47:22 -0800

    Added a query interface to handle the data access? so that i can search

---
+++ src/Query.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public interface Query {
+
+	public void writeJSON(Path path) throws IOException;
+	public void addQuery(String search, List<Result> results);
+	public boolean hasQuery(String query);
+	public String toString();
+
+}
