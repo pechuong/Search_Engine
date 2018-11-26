a6e85cb35e2986a93a9c10ce3bf88b8515528fca
peter <peter@10.0.0.6>
2018-11-25 16:24:09 -0800

    Removed the efficiency TODO comment issue and fixed sysout

--- src/ReadWriteLock.java
+++ src/ReadWriteLock.java
@@ -29,7 +29,7 @@ public class ReadWriteLock {
 				this.wait();
 			}
 		} catch (InterruptedException e) {
-			System.out.println("InterruptedException"); // TODO Fix
+			System.out.println("Thread " + this.toString() + " woke up in lockReadOnly()");
 		}
 		readers++;
 	}
@@ -43,22 +43,6 @@ public class ReadWriteLock {
 		if (readers <= 0) {
 			this.notifyAll();
 		}
-		
-		/*
-		 * TODO Efficency issue here---you are over-notifying. Assume there are
-		 * 10 active threads reading currently, i.e. readers == 10. If a writer
-		 * thread tries to lockReadWrite(), it is forced to wait for those readers.
-		 * 
-		 * Now suppose one of those readers calls unlockReadOnly. The number of
-		 * readers goes down to 9, and then the waiting writer is woken up. But,
-		 * it still can't write yet so it goes back to sleep.
-		 * 
-		 * And then suppose another reader calls unlockReadOnly. The number of 
-		 * readers goes down to 8, and the same thing happens again.
-		 * 
-		 * When should you really call notifyAll() so that you aren't unecessarily
-		 * waking up the writer thread?
-		 */
 	}
 
 	/**
@@ -71,7 +55,7 @@ public class ReadWriteLock {
 				this.wait();
 			}
 		} catch (InterruptedException e) {
-			System.out.println("InterruptedException"); // TODO Fix
+			System.out.println("Thread " + this.toString() + " woke up in lockReadWrite()");
 		} finally {
 			writers++;
 		}
