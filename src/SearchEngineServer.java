f055d9cad162960eed93d47a0778bf77810c1fff
peter <peter@10.0.0.6>
2018-12-20 12:37:54 -0800

    Filled in a test version of homeservlet to get it running

--- src/SearchEngineServer.java
+++ src/SearchEngineServer.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -27,7 +33,30 @@ public class SearchEngineServer {
 		server.join();
 	}
 
+	@SuppressWarnings("serial")
 	public static class HomeServlet extends HttpServlet {
+
+		private static final String TITLE = "Home";
+
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+
+			response.setContentType("text/html");
+
+			PrintWriter out = response.getWriter();
+
+			out.printf("<html>%n");
+			out.printf("<head><title>%s</title></head>%n", TITLE);
+			out.printf("<body>%n");
+
+			out.printf("<p>It is Wednesday my dudes.</p>%n");
+
+			out.printf("</body>%n");
+			out.printf("</html>%n");
+
+			response.setStatus(HttpServletResponse.SC_OK);
+		}
 
 	}
 
