16a09fa2c5425cf3f105f3162d75274ba763cdbc
Peter Chuong <peter@PC.local>
2019-06-27 14:37:03 -0700

    Cleaned up a lot of the code + found bug in result servlet

--- src/SearchEngineServer.java
+++ src/SearchEngineServer.java
@@ -31,10 +31,7 @@ public class SearchEngineServer {
 
 		this.server.addConnector(connector);
 		this.server.setHandler(handler);
-		this.server.start();
-	}
-
-	public void close() throws InterruptedException {
+		this.server.start(); // starting threads that stops when you hit stop
 		this.server.join();
 	}
 
@@ -47,9 +44,7 @@ public class SearchEngineServer {
 		protected void doGet(HttpServletRequest request, HttpServletResponse response)
 				throws ServletException, IOException {
 
-			//TODO Send the results to another servlet that displays the results
 			response.setContentType("text/html");
-
 			PrintWriter out = response.getWriter();
 
 			out.printf("<html>%n");
@@ -65,20 +60,15 @@ public class SearchEngineServer {
 			out.printf("<input type=\"submit\">%n");
 			out.printf("</form>%n");
 
-			//out.printf("%s", index.toString());
-
-			out.printf("</body>%n");
-			out.printf("</html>%n");
-
 			response.setStatus(HttpServletResponse.SC_OK);
 		}
-
 	}
 
 	public static String dayOfWeek() {
 		return Calendar.getInstance().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH);
 	}
 
+	@SuppressWarnings("serial")
 	public static class SearchResultServlet extends HttpServlet {
 
 		private static final String TITLE = "Results";
@@ -92,73 +82,18 @@ public class SearchEngineServer {
 		protected void doPost(HttpServletRequest request, HttpServletResponse response)
 				throws ServletException, IOException {
 			response.setContentType("text/html");
-			response.setStatus(HttpServletResponse.SC_OK);
-			response.sendRedirect("/results");
-
-			//PrintWriter out = response.getWriter();
-			//out.printf("%s", index.toString());
-			//this.index.writeIndex();
-			/*
 			PrintWriter out = response.getWriter();
 
 			out.printf("<html>%n");
 			out.printf("<head><title>%s</title></head>%n", TITLE);
 			out.printf("<body>%n");
-			out.printf("<p>It is %s my dudes.</p>%n", dayOfWeek());
-
-			out.printf("<h2>Welcome to my Search Engine</h2>%n");
-			 */
-			// TODO Show the results
-
-			//out.printf("</body>%n");
-			//out.printf("</html>%n");
-			// TODO Redirect to get method for this servlet
-		}
-
-		@Override
-		protected void doGet(HttpServletRequest request, HttpServletResponse response)
-				throws ServletException, IOException {
-			response.setContentType("text/html");
-			PrintWriter out = response.getWriter();
-
-			out.printf("<html>%n");
-			out.printf("<head><title>%s</title></head>%n", TITLE);
-			out.printf("<body>%n");
-			out.printf("<p>It is %s my dudes.</p>%n", dayOfWeek());
-			out.printf("<p>Index: %s", index.toString());
-			out.printf("<form method=\"POST\" action=\"/\">%n");
-			out.printf("<input type=\"submit\">< Go Back%n");
-			out.printf("</form>%n");
+			out.printf("<pre>%n");
+			out.printf(index.toString());
+			out.printf("</pre>%n");
+			out.printf("</body>%n");
 
 			response.setStatus(HttpServletResponse.SC_OK);
 		}
-
-		static void printSucessResult(HttpServletResponse response) throws IOException {
-			PrintWriter out = response.getWriter();
-
-			out.printf("<html>%n");
-			out.printf("<head><title>%s</title></head>%n", TITLE);
-			out.printf("<body>%n");
-
-			// Print the results
-			//out.printf("");
-
-
-		}
-
-		static void printFailedResult(HttpServletResponse response) throws IOException {
-			PrintWriter out = response.getWriter();
-
-			out.printf("<html>%n");
-			out.printf("<head><title>%s</title></head>%n", TITLE);
-			out.printf("<body>%n");
-
-			// Print the query word and then that there are no results
-			out.printf("");
-
-
-		}
-
 	}
 
 }
